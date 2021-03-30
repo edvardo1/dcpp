d8362487f8dfa6197acbcd73a58104f9e1c8e3f6
edvardo1 <eduardobeloni@mailfence.com>
2021-03-30 14:37:37 -0300

    add info to registers.h

---
+++ registers.h
@@ -0,0 +1,10 @@
+#ifndef REGISTERS_H
+#define REGISTERS_H
+
+/*
+ * TODO 
+ * implement registers with hashtables
+ * maybe use a library instead of reinventing the wheel?
+ */
+
+#endif /* REGISTERS_H */
